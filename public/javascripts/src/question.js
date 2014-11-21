d17d98a3f0c09fc81de04d657793adff046e3fd4
mackiso <ryo.mackiso@gmail.com>
2014-11-22 06:24:04 +0900

    use localStolage test.

--- public/javascripts/src/question.js
+++ public/javascripts/src/question.js
@@ -4,18 +4,29 @@ var s = io.connect('http://localhost:2000');
 s.on("connect", function () {});  // 接続時
 //s.on("disconnect", function (client) {});  // 切断時
 
-//サーバから受け取るイベント
-s.on("connect", function () {});  // 接続時
-//s.on("disconnect", function (client) {});  // 切断時
-
 //クライアントからイベント送信（イベント名は自由に設定できます）
+/*残骸
 function maru_batsu() {
     var msg = "maru_batsu"; //取得
     s.emit("maru_batsu", {value:msg}); //サーバへ送信
 }          
+*/
+
+for(var j = 0; j < getQuestionLength(); j++){
+    $question_text.append($("<li>").append(getQuestions(i)));
+}
+
+function getQuestionLength(){
+    var i=0; 
+    while(getQuestions(i)!==null){++i;} return i;}
+
+function getQuestions(i){return localStorage.getItem("question"+i); }
 
 $question_text = $("#question_text");
+
 s.on("question:send", function (data) {
+    i = getQuestionLength();
+    localStorage.setItem("question"+i,data.value);
     $question_text.append($("<li>").append(data.value));
 });
 
